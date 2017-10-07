a1b94a9fee3bffb9510a72a22ddfdf6547bbfea8
Christian Oder <myself5@carbonrom.org>
2017-10-07 17:03:37 +0200

    Adjust extract files to cover vendor partition
    Change-Id: I143dc205a7fb4ee2840523970317af9115f057b9

--- marlin/BoardConfigCarbon.mk
+++ marlin/BoardConfigCarbon.mk
@@ -1,2 +1,6 @@
 # BoardConfig for carbon_marlin
+
+TARGET_BOARD_INFO_FILE := device/google/marlin/marlin/vendor-board-info.txt
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
 include device/google/marlin/BoardConfigCarbon.mk
