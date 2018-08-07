880fdff4d96c1137d5a1451ac6b44db81e930a4b
shagbag913 <sh4gbag913@gmail.com>
2018-08-07 13:35:49 -0400

    marlin: Carbon-ify
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

---
+++ BoardConfigCarbon.mk
@@ -0,0 +1,8 @@
+# Custom board config for marlin/sailfish
+# Kernel
+TARGET_KERNEL_CONFIG := marlin_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_SOURCE := kernel/google/marlin
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 5.0.2
