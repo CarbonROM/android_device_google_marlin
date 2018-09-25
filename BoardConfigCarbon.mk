51cc59469f700660294455bb70ab9ab9c070a488
shagbag913 <sh4gbag913@gmail.com>
2018-09-25 18:14:48 -0400

    marlin: use carbon_marlin_defconfig
    Change-Id: Idcd5d1c282df1b3758843cf9894252b5a8a82c68
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -1,6 +1,6 @@
 # Custom board config for marlin/sailfish
 # Kernel
-TARGET_KERNEL_CONFIG := marlin_defconfig
+TARGET_KERNEL_CONFIG := carbon_marlin_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/google/marlin
